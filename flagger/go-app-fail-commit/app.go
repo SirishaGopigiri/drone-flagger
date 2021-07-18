e7681357612a22eb47344915de8d137d8d671c6c
Sirisha Gopigiri <sirishagopigiri@gmail.com>
2021-07-18 12:37:31 +0530

    Update repo commit

---
+++ flagger/go-app-fail-commit/app.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func RunServer() *gin.Engine {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(500, gin.H{
+			"message": "hello world!!",
+		})
+	})
+	r.GET("/newapi", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "New testing api added to the application!!",
+		})
+	})
+	return r
+}
+
+func main() {
+	RunServer().Run(":5000")
+}
+
